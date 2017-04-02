cfd4c629eeb52aa53703fd2588758e3f72f294b1
pierre-fromager <pf@pier-infor.fr>
2017-04-02 11:55:51 +0200

    bugfix/move public folder to up path

--- src/Pimvc/Interfaces/Layout.php
+++ src/Pimvc/Interfaces/Layout.php
@@ -10,7 +10,7 @@ namespace Pimvc\Interfaces;
 
 interface Layout {
 
-    const LAYOUT_PATH = '/public/layouts/';
+    const LAYOUT_PATH = '../public/layouts/';
     const LAYOUT_EXT = '.html';
     const LAYOUT_DEFAULT_NAME = 'reponsive';
 
