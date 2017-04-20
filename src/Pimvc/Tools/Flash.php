cb4f7a373c272d0b7b219df0f15bc7c35b358391
pierre-fromager <pf@pier-infor.fr>
2017-04-20 20:01:35 +0200

    feature/Tools Session+Flash

---
+++ src/Pimvc/Tools/Flash.php
@@ -0,0 +1,135 @@
+<?php
+
+
+/**
+ * Description of Pimvc\Tools\Flash
+ *
+ * @author pierrefromager
+ */
+namespace Pimvc\Tools;
+
+use Pimvc\Tools\Session as sessionTools;
+
+class Flash {
+
+    const FLASH_SESSION_KEY = 'flashdata';
+    const FLASH_ERROR = 'error';
+    const FLASH_WARNING = 'warning';
+    const FLASH_INFO = 'info';
+    const FLASH_HTTP_REDIRECT_CODE = 302;
+    
+    /**
+     * addInfo
+     * 
+     * @param string $message 
+     */
+    public static function addInfo($message) {
+        self::add(self::FLASH_INFO, $message);
+    }
+    
+    /**
+     * addWarning
+     * 
+     * @param string $message 
+     */
+    public static function addWarning($message) {
+        self::add(self::FLASH_WARNING, $message);
+    }
+
+    /**
+     * addError
+     * 
+     * @param string $message 
+     */
+    public static function addError($message) {
+        self::add(self::FLASH_ERROR, $message);
+    }
+
+    /**
+     * add
+     * 
+     * @param string $type
+     * @param string $message 
+     */
+    public static function add($type, $message) {
+        if (!empty($type) && !empty($message)) {
+            $flash = new \stdClass();
+            $flash->type = $type;
+            $flash->message = $message;
+            self::set($flash);
+        }
+    }
+    
+    /**
+     * render
+     * 
+     * @return string 
+     */
+    public static function render($helperName = 'Helper_Bootstrap_Flash') {
+        $flashes = self::get();
+        $helperRender = '';
+        if ($flashes) {
+            $helperRender = $helperName::get($flashes);
+            if (!self::isRedirect()) {
+                self::clean();
+            }
+        }
+        unset($flashes);
+        return $helperRender;
+    }
+
+    /**
+     * set
+     * 
+     * @param stdClass $flash 
+     */
+    private static function set($flash) {
+        $flashes = array_merge(self::get(),array($flash));
+        sessionTools::set(self::FLASH_SESSION_KEY, $flashes);
+    }
+    
+    /**
+     * clean
+     * 
+     */
+    private static function clean() {
+        if (self::has()) {
+            sessionTools::set(self::FLASH_SESSION_KEY, array());
+        }
+    }
+
+        /**
+     * has
+     * 
+     * @return boolean 
+     */
+    private static function has() {
+        return sessionTools::has(self::FLASH_SESSION_KEY);
+    }
+    
+    /**
+     * get
+     * 
+     * @return array 
+     */
+    private static function get() {
+        return (self::has()) 
+            ? sessionTools::get(self::FLASH_SESSION_KEY) 
+            : array();
+    }
+    
+    /**
+     * isRedirect
+     * 
+     */
+    private static function isRedirect() {
+        $redirect = false;
+        foreach(headers_list() as $header) {
+            if ($header == 'Status:302 Found'){
+                $redirect = true;break;
+            };
+        }
+        return $redirect;
+    }
+}
+
---
+++ src/Pimvc/Tools/Session.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+ * Description of Pimvc\Tools\Session
+ *  
+ * provides methods to get or set session datas.
+ *
+ * @author Pierre Fromager <pf@pier-infor.fr>
+ */
+
+namespace Pimvc\Tools;
+
+class Session {
+
+    const SESSION_ADMIN = 'admin';
+    const SESSION_MANAGER = 'manager';
+    const SESSION_ANONYMOUS = 'anonymous';
+    const SESSION_BASE_URL = 'baseurl';
+    const SESSION_ID = 'id';
+    const SESSION_PROFIL = 'profil';
+    const SESSION_USER_INFO = 'userinfo';
+
+    private static $baseUrl;
+
+    /**
+     * start
+     * 
+     */
+    public static function start() {
+        $baseUrl = \Pimvc\App::getInstance()->getRequest()->getBaseUrl();
+        session_name(md5($baseUrl));
+        session_start();
+    }
+
+    /**
+     * getUid
+     * 
+     * @return int 
+     */
+    public static function getUid() {
+        return (isset($_SESSION[self::SESSION_ID])) ? $_SESSION[self::SESSION_ID] : '';
+    }
+
+    /**
+     * isAuth
+     * 
+     * @return boolean 
+     */
+    public static function isAuth() {
+        $uid = self::getUid();
+        return (!empty($uid));
+    }
+
+    /**
+     * getProfil
+     * 
+     * @return string 
+     */
+    public static function getProfil() {
+        return (isset($_SESSION[self::SESSION_PROFIL])) ? $_SESSION[self::SESSION_PROFIL] : self::SESSION_ANONYMOUS;
+    }
+
+    /**
+     * getUserInfo
+     * 
+     * @return string 
+     */
+    public static function getUserInfo() {
+        return (isset($_SESSION[self::SESSION_USER_INFO])) ? $_SESSION[self::SESSION_USER_INFO] : self::SESSION_ANONYMOUS;
+    }
+
+    /**
+     * isAdmin
+     * 
+     * @return boolean 
+     */
+    public static function isAdmin() {
+        return (self::isAuth() && self::getProfil() == self::SESSION_ADMIN);
+    }
+
+    /**
+     * isManager
+     * 
+     * @return boolean 
+     */
+    public static function isManager() {
+        return (self::isAuth() && self::getProfil() == self::SESSION_MANAGER);
+    }
+
+    /**
+     * getBaseUrl
+     * 
+     * @return string 
+     */
+    public static function getBaseUrl() {
+        return $_SESSION[self::SESSION_BASE_URL];
+    }
+
+    /**
+     * isMine
+     * 
+     * @param int $uid
+     * @return boolean 
+     */
+    public static function isMine($uid) {
+        return (self::isAuth() && $uid == self::getUid());
+    }
+
+    /**
+     * set
+     * 
+     * @param string $name
+     * @param mixed $value 
+     * @param string $key 
+     */
+    public static function set($name, $value, $key = '') {
+        if ($key) {
+            $_SESSION[$name][$key] = $value;
+        } else {
+            $_SESSION[$name] = $value;
+        }
+    }
+
+    /**
+     * delete
+     * 
+     * @param string $name
+     * @param string $key 
+     */
+    public static function delete($name, $key = '') {
+        if ($key) {
+            unset($_SESSION[$name][$key]);
+        } else {
+            unset($_SESSION[$name]);
+        }
+    }
+
+    /**
+     * has
+     * 
+     * @param string $name
+     * @param string $key
+     * @return boolean 
+     */
+    public static function has($name, $key = '') {
+        $has = false;
+        if (empty($key)) {
+            $has = (isset($_SESSION[$name]) && !empty($_SESSION[$name]));
+        } else {
+            $has = (isset($_SESSION[$name][$key]) && !empty($_SESSION[$name][$key]));
+        }
+        return $has;
+    }
+
+    /**
+     * get
+     * 
+     * @param string $name
+     * @param string $key
+     * @return mixed 
+     */
+    public static function get($name, $key = '') {
+        if (empty($key)) {
+            $value = (self::has($name)) ? $_SESSION[$name] : '';
+        } else {
+            $value = (self::has($name, $key)) ? $_SESSION[$name][$key] : '';
+        }
+        return $value;
+    }
+
+    /**
+     * deAuth
+     * 
+     */
+    public static function deAuth() {
+        unset($_SESSION[self::SESSION_ID]);
+        unset($_SESSION[self::SESSION_PROFIL]);
+        unset($_SESSION[self::SESSION_USER_INFO]);
+    }
+
+    public static function __callStatic($name, $arguments) {
+        var_dump($name, $arguments);
+        /*
+          if (self::$instance === null) {
+          self::$instance = new self();
+          }
+
+          self::$instance->callStaticMethod($name, $arguments); */
+    }
+
+}
